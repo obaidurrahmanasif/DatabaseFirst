4ad34f45882844bcf1fbdb45794bad3878448a7f
saimonfaisalkhan <36556864+saimonfaisalkhan@users.noreply.github.com>
2018-03-21 20:27:09 +0600

    Add database

--- app/src/main/java/com/example/obaidurrahman/databasefirst/LogInActivity.java
+++ app/src/main/java/com/example/obaidurrahman/databasefirst/LogInActivity.java
@@ -1,13 +1,44 @@
 package com.example.obaidurrahman.databasefirst;
 
+import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LogInActivity extends AppCompatActivity {
-
+private EditText userid;
+private EditText password;
+private DatabaseHelper helper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        userid=findViewById(R.id.loginid);
+        password=findViewById(R.id.loginpass);
+        helper=new DatabaseHelper(getBaseContext());
+
+    }
+
+    public void loginClick(View view) {
+        String username=userid.getText().toString();
+        String pass=password.getText().toString();
+        boolean res=helper.islogin(new UserModel(0,username,null,pass));
+        if(res==true)
+        {
+            Intent intent=new Intent(LogInActivity.this,LoginNavActivity.class);
+            startActivity(intent);
+        }
+        else
+        {
+            Toast.makeText(getBaseContext(),"Login Failed!!",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void goRegister(View view) {
+        Intent intent=new Intent(LogInActivity.this,RegisterActivity.class);
+        startActivity(intent);
     }
 }
